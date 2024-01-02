7da03773ecc4fee8b397bded3d5a05d2cac3d91e
Steve Wagner <lanwin@lanwin.de>
2024-01-02 09:31:45 +0100

    add esp32 debug mqtt support - not yet tested since I have no ESP32

--- components/samsung_ac/debug_mqtt.cpp
+++ components/samsung_ac/debug_mqtt.cpp
@@ -3,8 +3,10 @@
 
 #ifdef USE_ESP8266
 #include <AsyncMqttClient.h>
-
 AsyncMqttClient *mqtt_client{nullptr};
+#endif
+#ifdef USE_ESP32
+esp_mqtt_client_handle_t mqtt_client{nullptr};
 #endif
 
 namespace esphome
@@ -18,7 +20,11 @@ namespace esphome
                 return false;
 
             return mqtt_client->connected();
+#elif USE_ESP32
+            if (mqtt_client == nullptr)
+                return false;
 #else
+
             return false;
 #endif
         }
@@ -36,6 +42,19 @@ namespace esphome
 
             if (!mqtt_client->connected())
                 mqtt_client->connect();
+#elif USE_ESP32
+            if (mqtt_client == nullptr)
+            {
+                std::string uri = "mqtt://";
+                if (username.length() > 0)
+                    uri = uri + username + ":" + password + "@";
+                uri = uri + host + ":" + std::to_string(port);
+                const esp_mqtt_client_config_t mqtt_cfg = {
+                    .broker.address.uri = uri.c_str(),
+                };
+                mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
+                esp_mqtt_client_start(mqtt_client);
+            }
 #endif
         }
 
@@ -46,6 +65,11 @@ namespace esphome
                 return false;
 
             return mqtt_client->publish(topic.c_str(), 0, false, payload.c_str()) != 0;
+#elif USE_ESP32
+            if (mqtt_client == nullptr)
+                return false;
+
+            return esp_mqtt_client_publish(mqtt_client, topic.c_str(), payload.c_str(), payload.length(), 0, false) != -1;
 #else
             return true;
 #endif
