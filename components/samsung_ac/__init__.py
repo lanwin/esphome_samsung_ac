eb4ce2864d179936f8fba5ac0b92b1bef31e05fa
north3221 <north3221@gmail.com>
2024-08-11 23:09:44 +0100

    fixing missing

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -382,7 +382,7 @@ async def to_code(config):
         if CONF_DEVICE_CUSTOM_BINARY in device:
             for cust_bin_sens in device[CONF_DEVICE_CUSTOM_BINARY]:
                 bin_sens = await binary_sensor.new_binary_sensor(cust_bin_sens)
-                cg.add(var_dev.add_custom_sensor(
+                cg.add(var_dev.add_custom_binary_sensor(
                     cust_bin_sens[CONF_DEVICE_CUSTOM_MESSAGE], bin_sens))
 
         for key in CUSTOM_SENSOR_KEYS:
