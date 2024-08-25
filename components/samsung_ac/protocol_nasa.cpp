1026ed0e2766837777e63f028881ca1a7ec379c3
Ömer Faruk ARAN <omerfaruk.aran@hotmail.com>
2024-08-25 16:36:26 +0300

    Added error code handling and message integration in protocol_nasa components

--- components/samsung_ac/protocol_nasa.cpp
+++ components/samsung_ac/protocol_nasa.cpp
@@ -714,7 +714,7 @@ namespace esphome
             }
             case MessageNumber::VAR_out_error_code:
             {
-                int code = ((int16_t)message.value);
+                int code = ((int32_t)message.value);
                 LOG_MESSAGE(VAR_out_error_code, code, source, dest);
                 target->set_error_code(source, code);
                 break;
