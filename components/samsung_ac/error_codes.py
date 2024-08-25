872bd71c6644765f1736db0dc5b2f8be4037eb3f
Ömer Faruk ARAN <omerfaruk.aran@hotmail.com>
2024-08-25 23:24:12 +0300

    Deleted error_codes.py

--- components/samsung_ac/error_codes.py
+++
@@ -1,4 +0,0 @@
-ERROR_CODES = {
-    201: "Communication error between indoor and outdoor units (installation number setting error,repeated indoor unit address, indoor unit communication cable error)",
-    # ...
-}
