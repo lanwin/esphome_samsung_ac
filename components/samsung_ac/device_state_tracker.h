d858ba9d89c128f3a39228d7c6027493a02faa39
Ömer Faruk ARAN <omerfaruk.aran@hotmail.com>
2024-08-30 17:00:06 +0300

    Fix device_state_tracker.h and device_state_tracker.cpp file create

--- components/samsung_ac/device_state_tracker.h
+++ components/samsung_ac/device_state_tracker.h
@@ -1,9 +1,6 @@
 #ifndef DEVICE_STATE_TRACKER_H
 #define DEVICE_STATE_TRACKER_H
 
-#include "esphome/core/log.h"
-#include "util.h"
-#include <vector>
 #include <map>
 #include <string>
 
@@ -13,35 +10,7 @@ class DeviceStateTracker {
   DeviceStateTracker(unsigned long timeout_period)
       : TIMEOUT_PERIOD(timeout_period) {}
 
-  void update(const std::string &address, const T &current_value) {
-    unsigned long now = millis();
-
-    if (pending_changes_.find(address) != pending_changes_.end()) {
-      if (current_value == pending_changes_[address]) {
-        pending_changes_.erase(address);
-      } else {
-        ESP_LOGI(TAG, "Stale value received for device: %s, ignoring.", address.c_str());
-        return;
-      }
-    }
-
-    if (last_values_.find(address) == last_values_.end() || last_values_[address] != current_value) {
-      pending_changes_[address] = current_value;
-      last_values_[address] = current_value;
-      last_update_time_[address] = now;
-
-      ESP_LOGI(TAG, "Value changed for device: %s", address.c_str());
-    } else {
-      ESP_LOGD(TAG, "No change in value for device: %s", address.c_str());
-
-      if (now - last_update_time_[address] > TIMEOUT_PERIOD) {
-        if (pending_changes_.find(address) != pending_changes_.end()) {
-          ESP_LOGW(TAG, "Timeout for device: %s, forcing update.", address.c_str());
-          pending_changes_.erase(address);
-        }
-      }
-    }
-  }
+  void update(const std::string &address, const T &current_value);
 
  private:
   std::map<std::string, T> last_values_;
