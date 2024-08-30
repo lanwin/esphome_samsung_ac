0b3a5bedbbdfdee3e491c4ca3a0c8043d2e574b2
Ömer Faruk ARAN <omerfaruk.aran@hotmail.com>
2024-08-30 16:15:08 +0300

    Fix log.h ESP_LOGI rebuild

--- test/esphome/core/log.h
+++ test/esphome/core/log.h
@@ -1,52 +1,32 @@
 #pragma once
-// Fake Log for Local Testing
+#include <cstdio>
+#include <string>
 
-namespace esphome
-{
+namespace esphome {
 
 #define ESP_LOGD(tag, format, ...)            \
-    do                                        \
-    {                                         \
-        std::string str = "";                 \
-        str += format;                        \
-        str += "\n";                          \
-        printf((str.c_str()), ##__VA_ARGS__); \
+    do {                                      \
+        printf("[%s] " format "\n", tag, ##__VA_ARGS__); \
     } while (0);
 
 #define ESP_LOGE(tag, format, ...)            \
-    do                                        \
-    {                                         \
-        std::string str = "";                 \
-        str += format;                        \
-        str += "\n";                          \
-        printf((str.c_str()), ##__VA_ARGS__); \
+    do {                                      \
+        printf("[%s] " format "\n", tag, ##__VA_ARGS__); \
     } while (0);
 
 #define ESP_LOGW(tag, format, ...)            \
-    do                                        \
-    {                                         \
-        std::string str = "";                 \
-        str += format;                        \
-        str += "\n";                          \
-        printf((str.c_str()), ##__VA_ARGS__); \
+    do {                                      \
+        printf("[%s] " format "\n", tag, ##__VA_ARGS__); \
     } while (0);
 
 #define ESP_LOGV(tag, format, ...)            \
-    do                                        \
-    {                                         \
-        std::string str = "";                 \
-        str += format;                        \
-        str += "\n";                          \
-        printf((str.c_str()), ##__VA_ARGS__); \
+    do {                                      \
+        printf("[%s] " format "\n", tag, ##__VA_ARGS__); \
     } while (0);
 
 #define ESP_LOGI(tag, format, ...)            \
-    do                                        \
-    {                                         \
-        std::string str = "";                 \
-        str += format;                        \
-        str += "\n";                          \
-        printf((str.c_str()), ##__VA_ARGS__); \
+    do {                                      \
+        printf("[%s] " format "\n", tag, ##__VA_ARGS__); \
     } while (0);
-    
+
 } // namespace esphome
